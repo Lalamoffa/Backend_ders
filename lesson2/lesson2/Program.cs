87ab3e6dc63debd67e5293adcb927328cb814c07
Lalamoffa <lalememmedova1133@gmail.com>
2024-02-19 17:03:50 +0400

    Add project files.

---
+++ lesson2/lesson2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin");
+            int simvol = Convert.ToInt32(Console.ReadLine());
+            if (simvol >= 91 && simvol <= 100)
+            {
+                Console.WriteLine("qiymet=A");
+            }
+            else if (simvol >= 81 && simvol <= 90)
+            {
+                Console.WriteLine("qiymet=B");
+            }
+            else if (simvol >= 71 && simvol <= 80)
+            {
+                Console.WriteLine("qiymet=C");
+            }
+            else if (simvol >= 61 && simvol <= 70)
+            {
+                Console.WriteLine("qiymet=D");
+            }
+            else if (simvol >= 51 && simvol <= 60)
+            {
+                Console.WriteLine("qiymet=E");
+            }
+            else
+            {
+                Console.WriteLine("qiymet= kesildinnnn");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+    
+
---
+++ mesgulluqBackend/mesgulluqBackend/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mesgulluqBackend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1ci ededi daxil edin:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Simvol daxil edin:");
+            char k = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("2ci ededi daxil edin:");
+            int b = Convert.ToInt32(Console.ReadLine());
+           
+
+
+            if (k == '-')
+            {
+                if (a < b)
+                {
+                    Console.WriteLine("netice: " + (b - a));
+                }
+            }
+            else if (k == '+')
+            {
+                Console.WriteLine("netice: " + a + b);
+            }
+            else if (k == '*')
+            {
+                Console.WriteLine("netice: " + a * b);
+            }
+
+            else if (k == '%')
+            {
+                Console.WriteLine("netice: " + a % b);
+            }
+
+            else if (k == '/')
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("0-a bolme emeli yoxdur. 0-dan ferqli eded daxil edin ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("netice: " + a / b);
+                }
+
+
+                else
+                {
+                    Console.WriteLine("netice: " + a / b);
+                }
+
+            }
+
+        }
+    }
+}
